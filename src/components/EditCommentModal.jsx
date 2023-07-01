e634ebe6a1ffae8e7b70fdbcdcbb3b3038fffd0c
Chaudhary-sonika <111914130+Chaudhary-sonika@users.noreply.github.com>
2023-07-01 18:29:09 +0530

    refactor: comment modal css

--- src/components/EditCommentModal.jsx
+++ src/components/EditCommentModal.jsx
@@ -1,30 +1,37 @@
 import { useState } from "react";
 import { useComment } from "../contexts/CommentContext";
 import "../components/cssComponent/EditCommentModal.css";
+import { useEffect } from "react";
 
 
 export const EditCommentmodal = ({setShowEditModal,  postId, comment})=>{
     const {editComments}= useComment();
     const [commentInput, setCommentInput] = useState({_id: comment?._id,
     comment: comment?.comment});
-    console.log(commentInput, "comm");
+    // console.log(commentInput, "comm");
+    useEffect(()=>{
+        document.body.style.overflowY = "hidden";
+        return()=>{
+           document.body.style.overflowY = "scroll";
+        };
+       }, []);
     return(
         <>
         <div className="editcomment_wrapper"></div>
         <div className="modal_container">
-            <div className="modal_header">
+            <div className="comm_modal_header">
                 <h2>Edit Comment:</h2>
-                <button className="btn_header" onClick={()=>setShowEditModal(false)}>X</button>
+                <button className="comm_btn_header" onClick={()=>setShowEditModal(false)}>X</button>
             </div>
-            <div className="modal_body">
+            <div className="comm_modal_body">
                 {commentInput?.comment && (
-                    <textarea className="edit_textarea" value={commentInput?.comment} onChange={(e)=> setCommentInput({...commentInput, comment: e.target.value})}></textarea>
+                    <textarea className="comm_edit_textarea" value={commentInput?.comment} onChange={(e)=> setCommentInput({...commentInput, comment: e.target.value})}></textarea>
                 )}
                 
             </div>
-            <div className="footer_div">
-            <button className="btn_save_cancel" onClick={()=>{editComments(postId, commentInput?._id, commentInput?.comment); setShowEditModal(false); }}>Save</button>
-            <button className="btn_save_cancel" onClick={()=>setShowEditModal(false)}>Cancel</button>
+            <div className="comm_footer_div">
+            <button className="comm_btn_save_cancel" onClick={()=>{editComments(postId, commentInput?._id, commentInput?.comment); setShowEditModal(false); }}>Save</button>
+            <button className="comm_btn_save_cancel" onClick={()=>setShowEditModal(false)}>Cancel</button>
             </div>
         </div>
         </>
